fe34ad987dc4dd8e3d98acc5a85908c99d5a1377
Vitaliy (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2021-12-26 19:52:52 +0300

    Remove callback_query fallback

--- src/botConstructor.ts
+++ src/botConstructor.ts
@@ -3,7 +3,6 @@ import { BotContext } from './types'
 import { Env } from './bindings'
 import { workerContext } from './telegramUtils'
 import { middlewares } from './tg-handlers'
-import { HAS_CALLBACK_QUERY_HANDLERS } from './tg-handlers/_events'
 
 let bot: Bot<BotContext>
 
@@ -33,11 +32,6 @@ export const getBot = (env?: Env, ctx?: ExecutionContext): Bot<BotContext> => {
   bot.use(workerContext<Env>(env, ctx))
 
   bot.use(...middlewares)
-
-  if (HAS_CALLBACK_QUERY_HANDLERS) {
-    // wildcard to handle unhandled requests
-    bot.on('callback_query:data', async (ctx) => ctx.answerCallbackQuery())
-  }
 
   return bot
 }
--- src/tg-handlers/_events.ts
+++ src/tg-handlers/_events.ts
@@ -5,9 +5,3 @@ export const UPDATES = new Set<UpdateType>()
 export const registerUpdatesSubscription = (event: UpdateType): void => {
   UPDATES.add(event)
 }
-
-export let HAS_CALLBACK_QUERY_HANDLERS = false
-
-export const registerCallbackQueryHandler = (): void => {
-  HAS_CALLBACK_QUERY_HANDLERS = true
-}
