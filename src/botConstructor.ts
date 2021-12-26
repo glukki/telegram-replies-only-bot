5f0391cfe7a61f8ef9ede31580968baebce8f6fc
Vitaliy (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2021-12-26 20:03:15 +0300

    Let bot get it's own info once

--- src/botConstructor.ts
+++ src/botConstructor.ts
@@ -16,18 +16,7 @@ export const getBot = (env?: Env, ctx?: ExecutionContext): Bot<BotContext> => {
     throw new Error("Can't init bot, missing env/ctx params")
   }
 
-  bot = new Bot<BotContext>(env.TELEGRAM_API_TOKEN, {
-    // TODO: let init once, or define via config, to make `/command@bot` work
-    botInfo: {
-      is_bot: true,
-      username: 'bot',
-      can_join_groups: true,
-      can_read_all_group_messages: false,
-      supports_inline_queries: false,
-      id: 1,
-      first_name: 'bot',
-    },
-  })
+  bot = new Bot<BotContext>(env.TELEGRAM_API_TOKEN)
 
   bot.use(workerContext<Env>(env, ctx))
 
